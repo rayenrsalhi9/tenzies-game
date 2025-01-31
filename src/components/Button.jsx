c2eaaa8fc51d94b177e7a2c877f3938ef8474828
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-01-31 07:12:59 +0100

    added a style when button is selected.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 import propTypes from 'prop-types';
 
 const Button = (props) => {
+    const styles = {
+        background: props.board?.selected && '#59E391',
+        transform: props.board?.selected && 'scale(1.1)'
+    }
     return(
-        <button className='dice' style={{background: props.board.selected && '#59E391'}} onClick={props.selectButton}>
+        <button className='dice' style={styles} onClick={props.selectButton}>
             {props.board.value}
         </button>
     );
